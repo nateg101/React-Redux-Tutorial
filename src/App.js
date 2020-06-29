99b159b08fd80fe1f54a0488763210cd64cf6f4a
Nat Elliott-Green <nat.elliott-green@avadolearning.com>
2020-06-29 15:00:24 +0100

    feat: added form so articles can be submitted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import List from './js/components/list';
+import Form from './js/components/form'
 
 const App = () => (
-  <div>
-    <h2>Articles</h2>
-    <List />
-  </div>
+  <>
+    <div>
+      <h2>Articles</h2>
+      <List />
+    </div>
+    <div>
+      <h2>Add a new article</h2>
+      <Form />
+    </div>
+  </>
 )
 
 export default App;

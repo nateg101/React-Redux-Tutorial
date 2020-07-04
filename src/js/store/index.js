df254c523595b3210a20405f6e03871c6fe47a8a
Nat Elliott-Green <natelliottgreen34@gmail.com>
2020-07-04 21:37:41 +0100

    feat: replaced redux-thunk with sagas to handle async in middleware

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,13 +1,19 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "../reducers/index";
-import { forbiddenWordsMiddleware } from "../middleware"
-import thunk from "redux-thunk";
+import { forbiddenWordsMiddleware } from "../middleware";
+import createSagaMiddleware from "redux-saga";
+import apiSaga from "../sagas/api-saga";
 
-
+const initialiseSagaMiddleware = createSagaMiddleware();
+const storeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
     rootReducer,
-    applyMiddleware(forbiddenWordsMiddleware, thunk)
-    );
+    storeEnhancers(
+      applyMiddleware(forbiddenWordsMiddleware, initialiseSagaMiddleware)
+    )
+);
+
+initialiseSagaMiddleware.run(apiSaga);
 
 export default store;
